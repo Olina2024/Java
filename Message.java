660580f9d4bfc6b721a4f9a1fa579bd95dbbc761
Olina2024 <ouyangjing1983@gmail.com>
2024-09-25 14:09:59 +0200

    Create Message.java

---
+++ Message.java
@@ -0,0 +1,30 @@
+import java.util.Timer;
+public class Message {
+    private String customer;
+    private String messages;
+    private int payment;
+   
+ 
+    public String getCustomer() {
+        return customer;
+    }
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+    
+    public String getMessages() {
+        return messages;
+    }
+    public void setMessages(String messages) {
+        this.messages = messages;
+    }
+   
+    public int getPayment() {
+        return payment;
+
+    }
+    public void setPayment(int payment) {
+        this.payment = payment;
+    }
+
+}
